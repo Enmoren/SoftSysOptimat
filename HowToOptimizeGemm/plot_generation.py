8c9f9df1f705432710a2916b25c122b5c4e7f9bf
coverney <cassandra.overney@students.olin.edu>
2019-03-18 15:41:22 -0400

    Generated new performance figure

--- HowToOptimizeGemm/plot_generation.py
+++ HowToOptimizeGemm/plot_generation.py
@@ -30,32 +30,33 @@ def analysisArray(stringArray):
 def plotData(pArray, gflopsArray):
 
     # Make a data frame
-    df=pd.DataFrame({'p': pArray, 'MMult0_2': gflopsArray[0],'MMult_1x4_9': gflopsArray[1], 'MMult_4x4_5': gflopsArray[2], 'MMult_4x4_15_2.txt':gflopsArray[3]})
+    df=pd.DataFrame({'p': pArray, 'MMult_4x4': gflopsArray[0],'MMult_1x4': gflopsArray[1], 'MMult0': gflopsArray[2]})
 
     # style
-    plt.style.use('seaborn-darkgrid')
+    # plt.style.use('seaborn-darkgrid')
 
     # create a color palette
-    palette = plt.get_cmap('Set1')
+    palette = ['tomato', 'orange', 'green']
 
     # multiple line plot
     num = 0
     for column in df.drop('p', axis=1):
+        plt.plot(df['p'], df[column], marker='', color=palette[num], linewidth=1, alpha=0.9, label=column)
         num += 1
-        plt.plot(df['p'], df[column], marker='', color=palette(num), linewidth=1, alpha=0.9, label=column)
 
     # Add legend
-    plt.legend(loc=5, ncol=1)
+    # plt.legend(loc=5, ncol=1)
+    plt.legend(loc='best')
 
     # Add titles
-    plt.title("Matix performance", loc='center', fontsize=12, fontweight=0, color='orange')
-    plt.xlabel("Leading Dimension")
-    plt.ylabel("Gflops/sec")
+    plt.title("Matrix Multiplication Performance", loc='center', fontsize=14, fontweight=0, color='black')
+    plt.xlabel("Leading Dimension", fontsize=12, labelpad=10)
+    plt.ylabel("Gflops/sec", fontsize=12, labelpad=10)
     plt.show()
 
 
 if __name__ == "__main__":
-    filenames = ['MMult0_2.txt','MMult_1x4_9_2.txt','MMult_4x4_5_2.txt','MMult_4x4_15_2.txt']
+    filenames = ['MMult_4x4_15.txt','MMult_1x4_9.txt','MMult0.txt']
     gflopsArray = []
     for file in filenames:
         generated_array = analysisStdin(file)
